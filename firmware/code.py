58e73a163281dcb343bc87e0e05267e875015f12
ananthunalloor <ananthunalloor@gmail.com>
2023-11-27 17:36:46 +0530

    lint

--- firmware/code.py
+++ firmware/code.py
@@ -25,40 +25,40 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 # key mapping
 keyboard.coord_mapping = [
-    0,  1,  2,  3,  4,  5,  6,  7,   47, 46, 45, 44, 43, 42, 41, 40,
-    8,  9,  10, 11, 12, 13, 14, 15,  55, 54, 53, 52, 51, 50, 49, 48,
-    16, 17, 18, 19, 20, 21, 22, 23,  63, 62, 61, 60, 59, 58, 57, 56,
-    24, 25, 26, 27, 28, 29, 30, 31,  71, 70, 69, 68, 67, 66, 65, 64,
-    32, 33, 34, 35, 36, 37, 38, 39,  79, 78, 77, 76, 75, 74, 73, 72
+    0,  1,  2,  3,  4,  5,  6,              46, 45, 44, 43, 42, 41, 40,
+    8,  9,  10, 11, 12, 13, 14,             54, 53, 52, 51, 50, 49, 48,
+    16, 17, 18, 19, 20, 21, 22, 23,     63, 62, 61, 60, 59, 58, 57, 56,
+    24, 25, 26, 27, 28, 29, 30, 31,     71, 70, 69, 68, 67, 66, 65, 64,
+    32, 33, 34, 35, 36, 37, 38, 39,     79, 78, 77, 76, 75, 74, 73, 72
 ]
 
 # ergodox keymap
 keyboard.keymap = [
     # base layer
     [   # Left layer                                                               #Right layer
-        KC.ESC,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,  KC.MINS, KC.NO,       KC.NO,   KC.EQL,  KC.N6,  KC.N7, KC.N8,    KC.N9,   KC.N0,   KC.GRAVE,
-        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,   KC.HYPR, KC.NO,       KC.NO,   KC.MEH,  KC.Y,   KC.U,  KC.I,     KC.O,    KC.P,    KC.BSLS,
+        KC.ESC,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,  KC.MINS,                       KC.EQL,  KC.N6,  KC.N7, KC.N8,    KC.N9,   KC.N0,   KC.GRAVE,
+        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,   KC.HYPR,                       KC.MEH,  KC.Y,   KC.U,  KC.I,     KC.O,    KC.P,    KC.BSLS,
         KC.CAPS, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,   KC.LT,   KC.PGUP,     KC.PGDN, KC.LT,   KC.H,   KC.J,  KC.K,     KC.L,    KC.SCLN, KC.QUOT,
         KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,   KC.HOME, KC.INS,      KC.PSCR, KC.END,  KC.N,   KC.M,  KC.COMMA, KC.DOT,  KC.SLSH, KC.RSHIFT,
         KC.LCTL, KC.LBRC, KC.LALT, KC.LEFT, KC.RGHT, KC.SPC, KC.LSFT, KC.HOME,     KC.DEL,  KC.BSPC, KC.ENT, KC.UP, KC.DOWN,  KC.LALT, KC.RBRC, KC.RCTRL
     ],
 
     # fn layer
-    [   # Left layer                                                               #Right layer
-        KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,  KC.F6,   KC.NO,       KC.NO,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.NO,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS
+    [   # Left layer                                                                #Right layer
+        KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,  KC.F6,   KC.NO,        KC.NO,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.NO,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS
     ],
 
     # media layer
     [   # Left layer                                                               #Right layer
-        KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.NO,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS
+        KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.NO,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,     KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS
     ]
 ]
 
